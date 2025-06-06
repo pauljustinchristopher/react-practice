b889adff3e3bf4a44989586895b22031e891b79c
paul justin christopher <pauljustinchristopher@gmail.com>
2025-06-06 02:31:26 +0100

    trail

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,12 @@
+import userList from "./userList";
 
 const App = () => {
-  const usersInfo = [
-    {
-      userName: "Paul",
-      email: "test@gmail.com",
-      location: "USA",
-    },
-    {
-      userName: "John",
-      email: "jd@gmail.com",
-      location: "Arab",
-    },
-    {
-      userName: "Justin",
-      email: "thy f@gmail.com",
-      location: "Niger",
-    }
-  ];
-
-  return ( 
-  <main>
-    { usersInfo.map(({ userName, email, location}) => (
-      <ul key={Math.random()}>
-        <li>{userName}</li>
-        <li>{email}</li>
-        <li>{location}</li>
-      </ul>
-    ))}
-  </main>
-
-  );
+  return (
+    <div>
+      <userList />
+    </div>
+  ); 
+  
 };
 
 export default App;
