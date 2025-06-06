718f052be53d5774bf3ac118eb9d839d3b7f775f
paul justin christopher <pauljustinchristopher@gmail.com>
2025-06-06 01:17:16 +0100

    Just trying to learn some things on react

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+
+const App = () => {
+  const usersInfo = [
+    {
+      userName: "Paul",
+      email: "test@gmail.com",
+      location: "USA"
+    },
+    {
+      userName: "John",
+      email: "jd@gmail.com",
+      location: "Arab"
+    }
+  ]
+
+  return ( 
+  <main>
+
+    {numbers.map((numbers) => (
+
+      <ul key={number}>
+
+        <li>{number}</li>
+
+      </ul>
+
+    ))}
+
+  </main>
+
+  );
+};
+
+export default App;
