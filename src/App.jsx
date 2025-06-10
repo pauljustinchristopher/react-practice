05df539a76a83efba688602614fd5e55d41d5716
paul justin christopher <pauljustinchristopher@gmail.com>
2025-06-10 23:43:42 +0100

    still on it

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
-import Product from "./component/Product";
-import Person from "./Person";
+const validPassword = () => <h1>Valid Password</h1>;
 
+const invalidPassword = () => <h1>Invalid Password</h1>;
+
+const passWord = ({ isValid }) => {
+  if(isValid) {
+    return <validPassword />;
+  }
+
+  return <invalidPassword />;
+}
 
 const App = () => {
   return (
-    <div>
-      <Person name="Paul" age={19} />
-      <Product name="Paulis" price={1234}/>
-    </div>
-  ); 
-  
-};
-
-export default App;
+    <section>
+    <passWord isValid = {true} />
+    </section>
+  );
+}; 
